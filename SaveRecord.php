ca3913eb3b79a9e8c1f67cb9fe25a1216d741e8f
Abhijeet Kumar <80936610+Abhijeetbyte@users.noreply.github.com>
2023-03-31 02:29:24 -0700

    Improved, bugfix, refactored
    ## improved
    
    * Added `flock()` : To stop concurrent access by multiple users, by obtaining an exclusive lock on the file handle, which prevents other users from accessing the file until the lock is released with
    
    *  The folder creation has been added and simplified by setting the recursive parameter to true in mkdir
    
    ## bugFix 
    
    * The `extract($_REQUEST)` function is removed, to avoid potential security risks
    
    ## Refactored 
    
    * Variable names are now in camelCase
    
    * The code for writing to the file has been simplified by passing an array to `fputcsv` instead of concatenating the values with commas

--- SaveRecord.php
+++ SaveRecord.php
@@ -1,43 +1,52 @@
 <?php
 
 
-//varible setting
- $Roll = $_REQUEST['roll_no']; 
- $pass = $_REQUEST['pass'];
- $S_name = $_REQUEST['student_name'];
- $S_grade = $_REQUEST['student_grade'];
- $S_marks = $_REQUEST['student_mark'];
- $Remark = $_REQUEST['remarks']; 
- 
+// Extract POST variables
+
+$rollNo = $_POST['roll_no']; // served as primery key
+$password = $_POST['pass'];
+$studentName = $_POST['student_name'];
+$studentGrade = $_POST['student_grade'];
+$studentMark = $_POST['student_mark'];
+$remarks = $_POST['remarks'];
 
 
- $password = 'asfr125g@83';   // Default password
+// Setting Variables
+
+$password = 'asfr125g@83'; // default password
+$dir = 'data'; // default folder name, files (.csv) store inside it
+$filename = $dir . '/' . $rollNo . '.csv'; // file naming acc. to Roll no.
 
 
- // check password
+// Validate password
+if ($password !== 'asfr125g@83') {
+    echo "<script>alert('Incorrect password, try again!');history.go(-1);</script>";
 
- if ($pass == $password){ 
-
-extract($_REQUEST);
-$file = fopen('data/'.$Roll.'.csv',"a");   // Create and Append data in respective CSV files acc. to Roll no. inside "data" folder
-
-fwrite($file, $Roll .",");
-fwrite($file, $S_name .",");
-fwrite($file, $S_grade .",");
-fwrite($file, $S_marks .",");
-fwrite($file, $Remark ."\n");
-    
-fclose($file);
-   
-echo"<script type='text/javascript'>alert('Submitted Successfully !');
-window.history.go(-1); 
-</script>";
-
-}else{     
-    echo"<script type='text/javascript'>alert('Incorrect password, try again !');  
-    window.history.go(-1); 
-    </script>";
-    exit(); // stop saving process
+    exit();  // Terminate the current script
 }
 
-?>
+
+// Create folder if it does not exist
+if (!file_exists($dir)) {
+    mkdir($dir, 0777, true);
+}
+
+
+// Open file in append mode (also creates , if file does not exists)
+
+$file = fopen($filename, 'a+');
+
+// Acquire exclusive lock on file to prevent concurrent access
+if (flock($file, LOCK_EX)) {
+
+    fputcsv($file, [$rollNo, $studentName, $studentGrade, $studentMark, $remarks]);
+
+    flock($file, LOCK_UN);
+
+
+    echo "<script>alert('Submitted successfully!');history.go(-1);</script>";
+} else {
+    echo "<script>alert('Unsuccessful ! Try after few minutes');history.go(-1);</script>";
+}
+
+fclose($file);
